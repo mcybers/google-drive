2dcdded40ff229427d236333258c6e4088cdee42
mcybers <mcyber@naver.com>
2022-11-09 10:28:23 +0900

    commit

---
+++ dashboard/src/main/java/google/drive/domain/FileUploaded.java
@@ -0,0 +1,15 @@
+package google.drive.domain;
+
+import google.drive.infra.AbstractEvent;
+import java.util.*;
+import lombok.Data;
+
+@Data
+public class FileUploaded extends AbstractEvent {
+
+    private Long id;
+    private String name;
+    private Long size;
+    private String type;
+    private String userId;
+}
